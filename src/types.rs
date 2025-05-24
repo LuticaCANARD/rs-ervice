ab9018a44056a7a6894437011e0f9ce54f9fa0fb
Lutica_CANARD <presan100@gmail.com>
2025-05-24 22:52:35 +0900

    초기 설정 파일 및 기본 구조 추가

---
+++ src/types.rs
@@ -0,0 +1,13 @@
+
+/// 하나의 서비스 컨테이너... 
+/// 서비스는 하나의 impl을 가지며, 다양한 함수들을 보유한다.
+pub struct Service<TActor> {
+    pub name: String,
+    pub cluster: Option<ServiceCluster>,
+    pub actor: TActor,
+}
+
+/// 하나의 서비스에서 관리할 이벤트들의 집합
+pub struct ServiceCluster{
+    pub services: Vec<Service<_>>,
+}
